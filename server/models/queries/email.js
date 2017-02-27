6cc294098c052c3f7dead32bd17be504787f7a2a
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-02-27 06:18:48 -0400

    fix config key (@materia/mailjet)

--- server/models/queries/email.js
+++ server/models/queries/email.js
@@ -4,12 +4,12 @@ class EmailModel {
 	constructor(app, entity) {
 		this.app = app;
 		this.entity = entity;
-		this.emailSender = new MailjetSender(app.addons.addonsConfig['mailjet'].apikey, app.addons.addonsConfig['mailjet'].secret, app.addons.addonsConfig['mailjet'].from, app.addons.addonsConfig['mailjet'].name)
+		this.emailSender = new MailjetSender(app.addons.addonsConfig['@materia/mailjet'].apikey, app.addons.addonsConfig['@materia/mailjet'].secret, app.addons.addonsConfig['@materia/mailjet'].from, app.addons.addonsConfig['@materia/mailjet'].name)
 	}
 
 	send(params) {
 		return this.emailSender.send(params).then(() => {
-			params.from = this.app.addons.addonsConfig['mailjet'].from
+			params.from = this.app.addons.addonsConfig['@materia/mailjet'].from
 			params.date_sent = new Date()
 			return this.entity.getQuery('create')
 				.run(params)
