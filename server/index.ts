4ea99104a0eaa7312b5b2e771684366e28ec8539
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-01-30 03:14:23 -0400

    (Setup) add setup in index.ts + move build to dist/server

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,41 @@
-const requireWithoutCache = (name) => {
-	let p = require.resolve(name)
+/* const requireWithoutCache = (name) => {
+	let p = require.resolve(name);
 	if (require.cache[p]) {
 			delete require.cache[p]
 	}
 	return require(name)
-}
+} */
 
 export class MailjetAddon {
 	public static displayName = "Mailjet";
 	public static logo = "https://pbs.twimg.com/profile_images/811557097758461952/Q679jb7i.jpg";
 
 	public static installSettings = [
+		{
+			name: "apikey",
+			description: "Enter your apikey",
+			type: "string",
+			required: true
+		},
+		{
+			name: "secret",
+			description: "Enter your secret key",
+			type: "string",
+			required: true
+		},
+		{
+			name: "from",
+			description: "Enter the email address (sender) attached with your apikey",
+			type: "string",
+			required: true,
+			component: "email"
+		},
+		{
+			name: "name",
+			description: "Enter the name you would like to see appear on your email",
+			type: "string",
+			required: true
+		}
 	];
 
 	constructor(app, config) { }
