0cdb5ee8adc8d30d3a4df04a54eb9708beea5465
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-02-18 06:44:08 -0400

    Use default export for server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@
 	return require(name)
 } */
 
-export class MailjetAddon {
+export default class MailjetAddon {
 	public static displayName = "Mailjet";
 	public static logo = "https://pbs.twimg.com/profile_images/811557097758461952/Q679jb7i.jpg";
 
