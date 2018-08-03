10eae3e6488e1fb9c4c31e30a83bcb1bb6392fc7
unknown <jeanmarc.royy@gmail.com>
2018-08-03 14:31:46 +0200

    (Server) Move template queries to mailjet-template entity + new mailjet-template.js query file

---
+++ server/models/queries/mailjet-template.js
@@ -0,0 +1,56 @@
+class MailjetTemplate {
+  constructor(app, entity) {
+    this.app = app;
+    this.entity = entity;
+    this.config = app.addons.addonsConfig['@materia/mailjet'];
+    if (this.config.apikey && this.config.secret) {
+      this.connect(this.config);
+    }
+  }
+
+  connect(config) {
+    this.mailjet = require('node-mailjet').connect(config.apikey, config.secret);
+  }
+
+  list(params) {
+    if (params.OwnerId) {
+      params.User = params.OwnerId;
+      delete params.OwnerId;
+    }
+    var templates = this.mailjet.get('template');
+    return templates.request(params)
+      .then((result) => result.body.Data)
+      .catch(err => err);
+  }
+
+  create(params) {
+    if (params.Purposes) {
+      params.Purposes = params.Purposes.split(',');
+    }
+    const createTemplate = this.mailjet.post('template');
+    return createTemplate.request(params)
+      .then((result) => result.body.Data)
+      .catch(err => err);
+  }
+
+  delete(params) {
+    const deleteTemplate = this.mailjet.delete(`template/${params.ID}`);
+    return deleteTemplate.request()
+    .then(() => true).catch(err => err);
+  }
+
+  updateContent(params) {
+    const updateContent = this.mailjet.post(`template/${params.ID}/detailcontent`);
+		delete params.ID;
+    return updateContent.request(params)
+    .then((result) => result.body.Data).catch(err => err);
+  }
+
+  getContent(params) {
+    const getContent = this.mailjet.get(`template/${params.ID}/detailcontent`);
+    return getContent.request()
+    .then((result) => result.body.Data).catch(err => err);
+  }
+}
+
+module.exports = MailjetTemplate;
