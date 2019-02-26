1c3a19975beb4451859ac1924efa3f0face65903
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-02-26 18:23:41 +0100

    (Client/MailjetView) add loading error template + refac init() method

--- client/components/mailjet-header/mailjet-header.component.ts
+++ client/components/mailjet-header/mailjet-header.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'mailjet-header',
   templateUrl: './mailjet-header.component.html',
-  styleUrls: ['./mailjet-header.component.scss']
+  styleUrls: ['./mailjet-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MailjetHeaderComponent {
   @Input() settings: any;
+  @Input() error: boolean;
   @Output() settingsOpened = new EventEmitter();
   @Output() sendDialogOpened = new EventEmitter();
   @Output() timelineChanged = new EventEmitter<string>();
--- client/components/mailjet-view/mailjet-view.component.ts
+++ client/components/mailjet-view/mailjet-view.component.ts
@@ -1,5 +1,12 @@
-
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, TemplateRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  TemplateRef
+} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material';
 import { DatePipe } from '@angular/common';
@@ -12,8 +19,7 @@ import { SendModalComponent } from '../../dialogs/send-modal/send-modal.componen
 @Component({
   selector: 'mailjet-view',
   templateUrl: './mailjet-view.component.html',
-  styleUrls: ['./mailjet-view.component.scss'],
-  providers: []
+  styleUrls: ['./mailjet-view.component.scss']
 })
 export class MailjetViewComponent implements OnInit {
   @Input() app;
@@ -59,12 +65,13 @@ export class MailjetViewComponent implements OnInit {
   emailsExpanded: boolean;
   statsProcessing: boolean;
   timeUnits: string[];
+  loadingError: boolean;
 
   constructor(
     private http: HttpClient,
     private dialog: MatDialog,
     private datePipe: DatePipe
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.statsExpanded = true;
@@ -77,7 +84,9 @@ export class MailjetViewComponent implements OnInit {
     this.sendSubject = '[TEST] Subject';
     this.sendType = type;
     this.sendModalComponent.refreshForm(type);
-    this.sendDialogRef = this.dialog.open(this.sendModalComponent.template, { panelClass: 'no-padding' });
+    this.sendDialogRef = this.dialog.open(this.sendModalComponent.template, {
+      panelClass: 'no-padding'
+    });
   }
 
   openSendToDialog(mail) {
@@ -85,7 +94,9 @@ export class MailjetViewComponent implements OnInit {
     this.sendTo = mail;
     this.sendType = 'simple';
     this.sendModalComponent.refreshForm();
-    this.sendDialogRef = this.dialog.open(this.sendModalComponent.template, { panelClass: 'no-padding' });
+    this.sendDialogRef = this.dialog.open(this.sendModalComponent.template, {
+      panelClass: 'no-padding'
+    });
   }
 
   send(data) {
@@ -96,28 +107,14 @@ export class MailjetViewComponent implements OnInit {
     }
   }
 
-  private _sendSimpleMessage(data) {
-    this.runQuery('mailjet_message', 'send', { subject: data.subject, body: data.body, to: data.to }).then(() => {
-      this.sendDialogRef.close();
-      this.snackbarSuccess.emit('Email successfully send');
-      this.reload();
-    }).catch(err => this.snackbarError.emit(err));
-  }
-
-  private _sendTemplateMessage(data) {
-    this.runQuery('mailjet_message', 'sendTemplate', { templateId: data.template, subject: data.subject, to: data.to }).then(() => {
-      this.sendDialogRef.close();
-      this.snackbarSuccess.emit('Email successfully send');
-      this.reload();
-    }).catch(err => this.snackbarError.emit(err));
-  }
-
   closeSendDialog() {
     this.sendDialogRef.close();
   }
 
   statsTimelineChange(timeline) {
-    this.init(timeline);
+    if (timeline && timeline === 'lastWeek' || timeline === 'lastMonth') {
+      this.init(timeline);
+    }
   }
 
   reload() {
@@ -128,50 +125,67 @@ export class MailjetViewComponent implements OnInit {
   init(timeline) {
     const fromTimestamp = this._getTimeline(timeline);
     this.statsProcessing = true;
-    this.getStats({ FromTS: fromTimestamp }).then(stats => {
-      if (stats && stats.length) {
-        stats = this._fillStats(stats, fromTimestamp);
-        this.data = [
-          this.getSerie('MessageSentCount', 'Sent', stats),
-          this.getSerie('MessageOpenedCount', 'Opened', stats),
-          this.getSerie('MessageClickedCount', 'Clicked', stats),
-          this.getSerie('MessageSpamCount', 'Spam', stats),
-          this.getSerie('MessageHardBouncedCount', 'Bounce', stats),
-          this.getSerie('MessageBlockedCount', 'Blocked', stats)
-        ];
-        this.stats[timeline] = this.data;
-        setTimeout(() => {
+    this.loadingError = false;
+    return this.getStats({ FromTS: fromTimestamp })
+      .then(stats => {
+        if (stats && stats.length) {
+          stats = this._fillStats(stats, fromTimestamp);
+          this.data = [
+            this.getSerie('MessageSentCount', 'Sent', stats),
+            this.getSerie('MessageOpenedCount', 'Opened', stats),
+            this.getSerie('MessageClickedCount', 'Clicked', stats),
+            this.getSerie('MessageSpamCount', 'Spam', stats),
+            this.getSerie('MessageHardBouncedCount', 'Bounce', stats),
+            this.getSerie('MessageBlockedCount', 'Blocked', stats)
+          ];
+          this.stats[timeline] = this.data;
+          setTimeout(() => {
+            this.statsProcessing = false;
+          }, 1000);
+        } else {
+          this.data = [];
           this.statsProcessing = false;
-        }, 1000);
-      } else {
-        this.data = [];
+        }
+        return this.runQuery('mailjet_message', 'list', {
+          FromTS: fromTimestamp,
+          limit: 1000
+        });
+      })
+      .then((messagesResponse: any) => {
+        this.emails = [...messagesResponse.data];
+        this.nbEmails = messagesResponse.count;
+        return this.runQuery('mailjet_contact', 'list', {
+          FromTS: fromTimestamp,
+          limit: 1000
+        });
+      })
+      .then((result: any) => {
+        this.contacts = result.data;
+        return this.getMailjetUser();
+      })
+      .then(() => {
+        this.loadTemplates();
+      })
+      .catch(err => {
+        this.loadingError = true;
         this.statsProcessing = false;
-      }
-    }).catch(err => {
-      this.snackbarError.emit(err);
-      this.statsProcessing = false;
-    });
-    this.runQuery('mailjet_message', 'list', { FromTS: fromTimestamp, limit: 1000 })
-      .then((response: any) => {
-        this.emails = [...response.data];
-        this.nbEmails = response.count;
       });
-    this.runQuery('mailjet_contact', 'list', { FromTS: fromTimestamp, limit: 1000 }).then((result: any) => {
-      this.contacts = result.data;
-    });
-    this.getMailjetUser().then(() => {
-      this.loadTemplates();
-    });
   }
 
   openMailjetTemplateEditor(template) {
-    this.openInBrowser.emit(`https://app.mailjet.com/template/${template.ID}/${template.EditMode === 1 ? 'build' : 'html'}`);
+    this.openInBrowser.emit(
+      `https://app.mailjet.com/template/${template.ID}/${
+        template.EditMode === 1 ? 'build' : 'html'
+      }`
+    );
   }
 
   openTemplateEditor(newTemplate) {
     this.newTemplate = newTemplate;
     this.templateEditor.refreshTemplateForm();
-    this.templateDialogRef = this.dialog.open(this.templateEditor.template, { panelClass: 'no-padding' });
+    this.templateDialogRef = this.dialog.open(this.templateEditor.template, {
+      panelClass: 'no-padding'
+    });
   }
 
   closeTemplateEditor() {
@@ -185,7 +199,9 @@ export class MailjetViewComponent implements OnInit {
   }
 
   loadTemplates() {
-    this.runQuery('mailjet_template', 'findAll', { OwnerId: this.mailjetUser.ID }).then((templateResult: any) => {
+    this.runQuery('mailjet_template', 'findAll', {
+      OwnerId: this.mailjetUser.ID
+    }).then((templateResult: any) => {
       this.templates = templateResult.data;
     });
   }
@@ -199,37 +215,46 @@ export class MailjetViewComponent implements OnInit {
   }
 
   private _createNewTemplate(data) {
-    this.runQuery('mailjet_template', 'create',
-      { Name: data.name, Author: data.author, Purposes: data.templateType, Locale: this.mailjetUser.Locale })
-      .then((result: any) => {
-        const newTemplate = result.data[0];
-        this.runQuery('mailjet_template', 'updateContent', {
-          ID: newTemplate.ID, 'Html-part': `<html>
+    this.runQuery('mailjet_template', 'create', {
+      Name: data.name,
+      Author: data.author,
+      Purposes: data.templateType,
+      Locale: this.mailjetUser.Locale
+    }).then((result: any) => {
+      const newTemplate = result.data[0];
+      this.runQuery('mailjet_template', 'updateContent', {
+        ID: newTemplate.ID,
+        'Html-part': `<html>
       <body>
         <h1 style="text-align: center; font-weight: 300; color: #0D47A1; margin: 20px;">
           Template generated via Materia Designer
         </h1>
         <p style=" font-size: 10px; margin-top: 50px; margin-left: 10px;">
           This email was sent to [[EMAIL_TO]],
-          <a style="text-decoration: none; color: inherit" href="[[UNSUB_LINK_${this.mailjetUser.Locale.split('_')[1]}]]" target="_blank">
+          <a style="text-decoration: none; color: inherit" href="[[UNSUB_LINK_${
+            this.mailjetUser.Locale.split('_')[1]
+          }]]" target="_blank">
           click here to unsubscribe
           </a>.
         </p>
       </body>
     </html>`,
-          Headers: { From: `${this.settings.name} <${this.settings.from}>`, Subject: 'Subject', 'Reply-To': this.settings.from }
-        })
-          .then((templateContent) => {
-            this.templateDialogRef.close();
-            this.loadTemplates();
-            this.openMailjetTemplateEditor(newTemplate);
-          });
+        Headers: {
+          From: `${this.settings.name} <${this.settings.from}>`,
+          Subject: 'Subject',
+          'Reply-To': this.settings.from
+        }
+      }).then(templateContent => {
+        this.templateDialogRef.close();
+        this.loadTemplates();
+        this.openMailjetTemplateEditor(newTemplate);
       });
+    });
   }
 
   private _fillStats(stats, fromTs) {
     const timeUnits = this._getTimeUnits(fromTs);
-    this.timeUnits = timeUnits.map((d) => {
+    this.timeUnits = timeUnits.map(d => {
       return this.datePipe.transform(d, 'shortDate');
     });
     const newStats = [];
@@ -246,7 +271,7 @@ export class MailjetViewComponent implements OnInit {
           newStats.push(stat);
         }
       });
-      if ( ! match) {
+      if (!match) {
         newStats.push({
           MessageSentCount: 0,
           MessageSpamCount: 0,
@@ -265,7 +290,8 @@ export class MailjetViewComponent implements OnInit {
     const firstDate = new Date(fromTs);
     const lastDate = new Date();
     const timeUnits = [];
-    const daysInterval = (lastDate.getTime() - firstDate.getTime()) / (60 * 60 * 24 * 1000);
+    const daysInterval =
+      (lastDate.getTime() - firstDate.getTime()) / (60 * 60 * 24 * 1000);
     for (let i = 0; i < daysInterval; i++) {
       const currentDate = new Date(fromTs);
       currentDate.setDate(firstDate.getDate() + i);
@@ -290,6 +316,34 @@ export class MailjetViewComponent implements OnInit {
     }
   }
 
+  private _sendSimpleMessage(data) {
+    this.runQuery('mailjet_message', 'send', {
+      subject: data.subject,
+      body: data.body,
+      to: data.to
+    })
+      .then(() => {
+        this.sendDialogRef.close();
+        this.snackbarSuccess.emit('Email successfully send');
+        this.reload();
+      })
+      .catch(err => this.snackbarError.emit(err));
+  }
+
+  private _sendTemplateMessage(data) {
+    this.runQuery('mailjet_message', 'sendTemplate', {
+      templateId: data.template,
+      subject: data.subject,
+      to: data.to
+    })
+      .then(() => {
+        this.sendDialogRef.close();
+        this.snackbarSuccess.emit('Email successfully send');
+        this.reload();
+      })
+      .catch(err => this.snackbarError.emit(err));
+  }
+
   private runQuery(entity: string, query: string, params?: any) {
     return this.http
       .post(`${this.baseUrl}/entities/${entity}/queries/${query}`, params)
@@ -297,18 +351,29 @@ export class MailjetViewComponent implements OnInit {
   }
 
   private getMailjetUser() {
-    return this.runQuery('mailjet_user', 'list').then((result: { count: number, data: any }) => {
-      this.mailjetUser = result.data[0];
-      return this.mailjetUser;
-    });
+    return this.runQuery('mailjet_user', 'list').then(
+      (result: { count: number; data: any }) => {
+        this.mailjetUser = result.data[0];
+        return this.mailjetUser;
+      }
+    );
   }
 
   private getStats(params?) {
-    const queryParams = params ?
-      Object.assign({}, params, { CounterResolution: 'Day', CounterTiming: 'Message', CounterSource: 'APIKey' })
-      : { CounterResolution: 'Day', CounterTiming: 'Message', CounterSource: 'APIKey' };
-    return this.runQuery('mailjet_statistic', 'find', queryParams)
-      .then((result: { count: number, data: any }) => result.data);
+    const queryParams = params
+      ? Object.assign({}, params, {
+          CounterResolution: 'Day',
+          CounterTiming: 'Message',
+          CounterSource: 'APIKey'
+        })
+      : {
+          CounterResolution: 'Day',
+          CounterTiming: 'Message',
+          CounterSource: 'APIKey'
+        };
+    return this.runQuery('mailjet_statistic', 'find', queryParams).then(
+      (result: { count: number; data: any }) => result.data
+    );
   }
 
   private getSerie(name, displayName, data) {
