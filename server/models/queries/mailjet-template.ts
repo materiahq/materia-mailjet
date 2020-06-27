6a85a555871c535c7d713a51237dfa82902c47a1
GeoAstronaute <jeanmarc.royy@gmail.com>
2020-06-27 17:34:14 +0200

    fix(server): retrieve templates list with axios and api rest url

--- server/models/queries/mailjet-template.ts
+++ server/models/queries/mailjet-template.ts
@@ -1,70 +1,56 @@
-import { App } from '@materia/server';
 
-import { MailjetSender } from '../../lib/mailjet';
+import { App } from '@materia/server';
+import axios from 'axios';
+import { MailjetService } from '../../lib/mailjet';
 
 class MailjetTemplate {
-  mailjetLib: MailjetSender;
+  mailjetLib: MailjetService;
 
   constructor(private app: App) {
-    this.mailjetLib = new MailjetSender(this.app);
+    this.mailjetLib = new MailjetService(this.app);
   }
 
-  list(params) {
-    this.mailjetLib.reload();
-    if (this.mailjetLib.mailjet) {
-      const templates = this.mailjetLib.mailjet.get('template');
-      return templates.request(params).then(result => result.body.Data);
-    } else {
-      return Promise.reject(new Error('Addon @materia/mailjet not configured'));
-    }
+  async list(params) {
+    this.mailjetLib.checkApi();
+    // Use directly api REST because of issue with node mailjet on this endpoint
+    // const templatesEndpoint = this.mailjetLib.api.get('template', {'version': 'v3'});
+    const buffer = new Buffer(`${MailjetService.apikey}:${MailjetService.secret}`);
+    const { data: { Data } } = await axios.get('https://api.mailjet.com/v3/REST/template', { headers: {
+      Authorization: `Basic ${buffer.toString('base64')}`
+    }})
+    return Data;
   }
 
   create(params) {
-    this.mailjetLib.reload();
-    if (this.mailjetLib.mailjet) {
-      if (params.Purposes) {
-        params.Purposes = params.Purposes.split(',');
-      }
-      const createTemplate = this.mailjetLib.mailjet.post('template');
-      return createTemplate.request(params).then(result => result.body.Data);
-    } else {
-      return Promise.reject(new Error('Addon @materia/mailjet not configured'));
+    this.mailjetLib.checkApi();
+    if (params.Purposes) {
+      params.Purposes = params.Purposes.split(',');
     }
+    const createTemplateEndpoint = this.mailjetLib.api.post('template');
+    return createTemplateEndpoint.request(params).then(({ body: { Data } }) => Data);
   }
 
   delete(params) {
-    this.mailjetLib.reload();
-    if (this.mailjetLib.mailjet) {
-      const deleteTemplate = this.mailjetLib.mailjet.delete(`template/${params.ID}`);
-      return deleteTemplate.request().then(() => true);
-    } else {
-      return Promise.reject(new Error('Addon @materia/mailjet not configured'));
-    }
+    this.mailjetLib.checkApi();
+    const deleteTemplateRequest = this.mailjetLib.api.delete(`template/${params.ID}`).request();
+    return deleteTemplateRequest.then(() => true);
   }
 
   updateContent(params) {
-    this.mailjetLib.reload();
-    if (this.mailjetLib.mailjet) {
-      const updateContent = this.mailjetLib.mailjet.post(
-        `template/${params.ID}/detailcontent`
-      );
-      delete params.ID;
-      return updateContent.request(params).then(result => result.body.Data);
-    } else {
-      return Promise.reject(new Error('Addon @materia/mailjet not configured'));
-    }
+    this.mailjetLib.checkApi();
+    const updateContentEndpoint = this.mailjetLib.api.post(
+      `template/${params.ID}/detailcontent`
+    );
+    delete params.ID;
+    return updateContentEndpoint.request(params).then(({ body: { Data } }) => Data);
   }
 
   getContent(params) {
-    this.mailjetLib.reload();
-    if (this.mailjetLib.mailjet) {
-      const getContent = this.mailjetLib.mailjet.get(
-        `template/${params.ID}/detailcontent`
-      );
-      return getContent.request().then(result => result.body.Data);
-    } else {
-      return Promise.reject(new Error('Addon @materia/mailjet not configured'));
-    }
+    this.mailjetLib.checkApi();
+    const getContentRequest = this.mailjetLib.api.get(
+      `template/${params.ID}/detailcontent`
+    ).request();
+    return getContentRequest.then(({ body: { Data } }) => Data);
   }
 }
 
